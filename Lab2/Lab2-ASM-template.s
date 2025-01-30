faa670abf66dc0dcf7ef99755f6115b1b57884a9
Matthew Herber <Matthewherber@sbcglobal.net>
2025-01-30 09:01:34 -0500

    implement stack saving

--- Lab2/Lab2-ASM-template.s
+++ Lab2/Lab2-ASM-template.s
@@ -18,26 +18,36 @@
 findHighBitASM
 	; where do you expect the subroutine input parameters to be passed in?
 	; input parameters: R0: address of array, R1: number of elements in the array
+	
 	;Define the inputs
 	#define arrPtr R0
 	#define arrLen R1
+	
+	;Define the outputs
 	#define returnLocation R0
+	
+	;Defined local vars
+	#define arrIndex R2
+	#define highestBit R3
+	; According to the ABI for 32 bit arm processors, only r0-r3 are scratch registers, so everything else needs to be saved and restored once we are finished
+	#define leadingZeros R4
+	#define valueAtIndex R5
 
-	;PUSH ...		; do you need to preserve registers that you may end up using ?
+	; Define our stack saving list
+	#define savedStack {valueAtIndex, leadingZeros}
+
+	; Push all registers that are used
+	PUSH savedStack
 
 	;use R3 as a bit counter, initialize to 255 to assume the high bit of the array is the MSB
-	#define highestBit R3
 	LDR highestBit,=255
-	#define arrIndex R2
 	MOV arrIndex, R1 ;copy R1 to R2 to act as our active index into the array
 
 loop1
 	; Subtract 1 in order to get a proper index into the array
 	SUB arrIndex, arrIndex, #1
 	
-	#define valueAtIndex R5
 	LDR valueAtIndex, [arrPtr, arrIndex, LSL #2]; ; Load the current int into valueAtIndex, using arrIndex as a index into the array. Multiply arrIndex by 4 due to addressing being in bytes instead of words
-	#define leadingZeros R4
 	CLZ leadingZeros, valueAtIndex ;Count the leading zeros into the leadingZeros register
 	;Result will be 32 if no bits are set, or 0 if bit 31 is set
 	;We can directly subtract this from highestBit which holds the bit position
@@ -53,7 +63,9 @@ loop1
 done 
 	
 	MOV returnLocation, highestBit	; make sure that the final result is loaded in R0
-	;POP ...			; restore saved registers
+	; restore saved registers
+	POP savedStack
+	
 	BX LR			; return to the caller
 
 	END
